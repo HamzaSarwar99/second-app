bf9ef6787aeb46b34ba70bed2cdf1fb4119797cb
Hamza Sarwar <hamzasarwarofficial99@gmail.com>
2024-09-17 14:11:04 +0500

    [feat] - create ui design

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import FlatCards from './Components/FlatCards';
+import ElevatedCards from './Components/ElevatedCards';
+import FancyCards from './Components/FancyCards';
+import ActionCard from './Components/ActionCard';
+import ContactList from './Components/ContactList';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <FlatCards />
+          <ElevatedCards />
+          <FancyCards />
+          <FancyCards />
+          <FancyCards />
+          <ActionCard />
+          <ContactList/>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
